02a86100003f423ea063a7d987b7f9fcda5d397e
Chanmoey <relijifa@gmail.com>
2021-10-23 12:03:40 +0800

    2021/10/23

---
+++ src/LeetCode154/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode154;
+
+class Solution {
+    public int findMin(int[] nums) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return nums[i + 1];
+            }
+        }
+
+        return nums[0];
+    }
+}
