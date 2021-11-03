c6d308c404c184e900337313e7d2d5b0ff5a78e6
Chanmoey <relijifa@gmail.com>
2021-11-03 10:55:27 +0800

    2021/11/3

---
+++ src/LeetCode129/Solution1.java
@@ -0,0 +1,30 @@
+package LeetCode129;
+
+import TreeNode.TreeNode;
+
+class Solution1 {
+
+    private int res = 0;
+
+    public int sumNumbers(TreeNode root) {
+
+        this.sumNumbers(root, 0);
+        return this.res;
+    }
+
+    private void sumNumbers(TreeNode root, int lastSum) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.right == null && root.left == null) {
+            this.res += lastSum * 10 + root.val;
+            return;
+        }
+
+        lastSum = lastSum * 10 + root.val;
+
+        sumNumbers(root.right, lastSum);
+        sumNumbers(root.left, lastSum);
+    }
+}
