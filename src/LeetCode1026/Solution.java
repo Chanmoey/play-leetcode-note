b793f267f898c63bf4ffbc8b0327b256d2c52a45
Chanmoey <relijifa@gmail.com>
2021-12-31 09:40:26 +0800

    2021/12/31 2021最后一天

---
+++ src/LeetCode1026/Solution.java
@@ -0,0 +1,34 @@
+package LeetCode1026;
+
+import com.sun.source.tree.Tree;
+import treenode.TreeNode;
+
+/**
+ * @author Chanmoey
+ * @date 2021年12月31日
+ */
+class Solution {
+
+    private int maxValue = 0;
+
+    public int maxAncestorDiff(TreeNode root) {
+        this.findMaxValue(root, root, root);
+        this.findMaxValue(root, root, root);
+        return maxValue;
+    }
+
+    private void findMaxValue(TreeNode node, TreeNode maxNode, TreeNode minNode) {
+
+        if (node == null) {
+            return;
+        }
+
+        maxNode = maxNode.val > node.val ? maxNode : node;
+        minNode = minNode.val < node.val ? minNode : node;
+
+        this.maxValue = Math.max(this.maxValue, Math.abs(maxNode.val - minNode.val));
+
+        findMaxValue(node.left, maxNode, minNode);
+        findMaxValue(node.right, maxNode, minNode);
+    }
+}
