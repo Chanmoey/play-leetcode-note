ca68007675245b82102636389b48c2175b70d157
Chanmoey <relijifa@gmail.com>
2022-01-10 19:06:38 +0800

    2022/1/10

---
+++ src/LeetCode617/Solution.java
@@ -0,0 +1,28 @@
+package LeetCode617;
+
+import treenode.TreeNode;
+
+/**
+ * @author Chanmoey
+ * @date 2022年01月10日
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return null;
+        }
+
+        if (root1 == null) {
+            return root2;
+        }
+
+        if (root2 == null) {
+            return root1;
+        }
+
+        TreeNode root = new TreeNode(root1.val + root2.val);
+        root.left = mergeTrees(root1.left, root2.left);
+        root.right = mergeTrees(root1.right, root2.right);
+        return root;
+    }
+}
