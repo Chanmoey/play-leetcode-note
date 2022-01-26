f8c1579cf5581a93d1188f10b8b183ac94c43740
Chanmoey <relijifa@gmail.com>
2022-01-26 10:52:24 +0800

    2022/1/26

--- src/LeetCode387/Solution1.java
+++ src/LeetCode387/Solution1.java
@@ -6,7 +6,7 @@ import java.util.Map;
  * @author Chanmoey
  * @date 2022年01月26日
  */
-class Solution {
+class Solution1 {
     public int firstUniqChar(String s) {
         int index = Integer.MAX_VALUE;
         for (char i = 'a'; i <= 'z'; i++) {
