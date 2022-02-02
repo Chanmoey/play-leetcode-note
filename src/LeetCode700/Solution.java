17de0686a1b7408a0adda55b3476dbfb74f0835c
Chanmoey <relijifa@gmail.com>
2022-02-02 10:25:28 +0800

    2022/2/2

---
+++ src/LeetCode700/Solution.java
@@ -0,0 +1,25 @@
+package LeetCode700;
+
+import treenode.TreeNode;
+
+/**
+ * @author Chanmoey
+ * @date 2022年02月02日
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        }
+
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
