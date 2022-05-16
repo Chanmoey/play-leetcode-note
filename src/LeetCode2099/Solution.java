6cf1d00db4107d98db2cdd4d4d22c42c967d0f48
Chanmoey <relijifa@gmail.com>
2022-05-16 09:45:51 +0800

    2022/5/16

---
+++ src/LeetCode2099/Solution.java
@@ -0,0 +1,42 @@
+package LeetCode2099;
+
+import javax.print.DocFlavor;
+import java.util.*;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月16日
+ */
+class Solution {
+    public int[] maxSubsequence(int[] nums, int k) {
+        if (k >= nums.length) {
+            return nums;
+        }
+        int[] arr = nums.clone();
+        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>(2 * k);
+
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < k) {
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+            j--;
+            i++;
+        }
+
+        int[] res = new int[k];
+        i = 0;
+        for (int num : arr) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                res[i] = num;
+                i++;
+                map.put(num, map.get(num) - 1);
+                if (i == k) {
+                    return res;
+                }
+            }
+        }
+
+        return res;
+    }
+}
