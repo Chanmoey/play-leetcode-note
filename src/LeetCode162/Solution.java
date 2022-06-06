4ed6b126527beb6ee51a9accf80d0494b93d258f
Chanmoey <relijifa@gmail.com>
2022-06-06 08:16:02 +0800

    2022/6/6

---
+++ src/LeetCode162/Solution.java
@@ -0,0 +1,22 @@
+package LeetCode162;
+
+/**
+ * @author Chanmoey
+ * @date 2022年06月06日
+ */
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
