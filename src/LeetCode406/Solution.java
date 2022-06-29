6e36fbdb50535a340f2631ee31787f7598ba2ab5
Chanmoey <relijifa@gmail.com>
2022-06-29 19:32:35 +0800

    2022/6/28

---
+++ src/LeetCode406/Solution.java
@@ -0,0 +1,22 @@
+package LeetCode406;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Chanmoey
+ * @date 2022年06月29日
+ */
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+
+        List<int[]> ordered = new LinkedList<>();
+        for (int[] p: people) {
+            ordered.add(p[1], p);
+        }
+
+        return ordered.toArray(new int[people.length][2]);
+    }
+}
