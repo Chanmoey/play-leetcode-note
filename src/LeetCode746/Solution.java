d5bc2eb6de1aff00d8fc919229ebe2570262fcd4
Chanmoey <relijifa@gmail.com>
2022-07-10 18:38:04 +0800

    2022/7/10

---
+++ src/LeetCode746/Solution.java
@@ -0,0 +1,24 @@
+package LeetCode746;
+
+import java.util.Map;
+
+/**
+ * @author Chanmoey
+ * @date 2022年07月10日
+ */
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+
+        if (n == 2) {
+            return Math.min(cost[0], cost[1]);
+        }
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+
+        return dp[n];
+    }
+}
