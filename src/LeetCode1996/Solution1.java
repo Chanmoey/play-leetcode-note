612d90869effaf0c2a00e4d70becce9df2b93520
Chanmoey <relijifa@gmail.com>
2022-09-09 09:27:19 +0800

    2022/9/9

--- src/LeetCode1996/Solution1.java
+++ src/LeetCode1996/Solution1.java
@@ -20,15 +20,22 @@ class Solution1 {
 
         Arrays.sort(properties, (a, b) -> {
             if (a[0] == b[0]) {
-                return a[1] - b[1];
+                // 防御力倒序排序
+                return b[1] - a[1];
             } else {
-                return b[0] - a[0];
+                // 攻击力正序排序
+                return a[0] - b[0];
             }
         });
+
+        // 排序之前：[[1, 5], [10, 4], [4, 3]]
+        // 排序之后：[[1, 5], [4, 3], [10, 4]]
 
         int weak = Integer.MIN_VALUE;
         int res = 0;
 
+        // 从后往前看，我们记录的 weak 其实就是防御值，但是这个防御值的攻击力一定是大于前面的。
+        // 所以从后往前查询时，看到有防御值比 weak 还小，那就是一个严格的 weak character。
         for (int i = n - 1; i >= 0; i--) {
             if (properties[i][1] < weak) {
                 res ++;
