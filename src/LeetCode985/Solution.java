066cd9ef6ffc63dd7017ac136320488a62e62fc4
Chanmoey <relijifa@gmail.com>
2022-09-21 15:04:11 +0800

    2022/9/21

---
+++ src/LeetCode985/Solution.java
@@ -0,0 +1,49 @@
+package LeetCode985;
+
+/**
+ * @author Chanmoey
+ * @date 2022年09月21日
+ */
+class Solution {
+    public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
+        int evenSum = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                evenSum += num;
+            }
+        }
+
+        int[] res = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int val = queries[i][0];
+            int idx = queries[i][1];
+
+            // 奇数
+            if (Math.abs(val) % 2 == 1) {
+
+                // 原来位置也是奇数，那么就会出现新的偶数
+                if (Math.abs(nums[idx]) % 2 == 1) {
+                    nums[idx] += val;
+                    evenSum += nums[idx];
+                } else { // 原数组的位置是偶数，那么原数组的位置就会变成奇数
+                    evenSum -= nums[idx];
+                    nums[idx] += val;
+                }
+
+            } else { // 偶数
+                // 原数组的位置是奇数，那么原数组的位置就会变成奇数
+                if (Math.abs(nums[idx]) % 2 == 1) {
+                    nums[idx] += val;
+                } else { // 原数组的位置是偶数，那么原数组的位置就会变成新的偶数
+                    nums[idx] += val;
+                    evenSum += val;
+                }
+
+            }
+            res[i] = evenSum;
+        }
+
+        return res;
+    }
+}
